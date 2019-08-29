713dd4768a984a7229ced89e2ca3a291112d0eec
Jaiden <jaidenh16@gmail.com>
2019-08-29 13:25:32 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: jaiden holbert, Osama Al Sabahi
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("jaiden jolbert, osama Al Sabahi\n");
+
+  return 0;
+}
